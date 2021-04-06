1fd0a901f9146c92e49956015d9439dc84df9c72
HatsOnLamps <81886905+HatsOnLamps@users.noreply.github.com>
2021-04-05 21:53:58 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -36,9 +36,9 @@ addLayer("B", {
 		points: new Decimal(0),
     }},
     color: "#32a148",
-    requires: new Decimal(5), // Can be a function that takes requirement increases into account
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "big borii", // Name of prestige currency
-    baseResource: "borii", // Name of resource prestige is based on
+    baseResource: "", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
