f349f2e173f89a49d7d8ab90e1d8740195f548c2
HatsOnLamps <81886905+HatsOnLamps@users.noreply.github.com>
2021-04-05 23:17:03 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -16,6 +16,7 @@ addLayer("b", {
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         if (hasUpgrade("b", 12)) mult = mult.times(2)
+        mult = mult.times(2)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
