663bb9e25cc83d684e4f25ce197177e524324808
HatsOnLamps <81886905+HatsOnLamps@users.noreply.github.com>
2021-04-05 21:44:00 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,5 +1,5 @@
 addLayer("b", {
-    name: "beans", // This is optional, only used in a few places, If absent it just uses the layer id.
+    name: "borii", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "b", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
@@ -23,6 +23,35 @@ addLayer("b", {
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "b", description: "B: reset to spawn Borii", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true}
+})
+
+addLayer("B", {
+    name: "big borii", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "B", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#32a148",
+    requires: new Decimal(5), // Can be a function that takes requirement increases into account
+    resource: "big borii", // Name of prestige currency
+    baseResource: "borii", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "B", description: "shift-B: reset to merge and form Big Boris", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
 })
