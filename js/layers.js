d5daf4dff9da20bba77afdca91f5df34269e1f94
HatsOnLamps <81886905+HatsOnLamps@users.noreply.github.com>
2021-04-05 21:59:59 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -9,7 +9,7 @@ addLayer("b", {
     color: "#32a148",
     requires: new Decimal(5), // Can be a function that takes requirement increases into account
     resource: "borii", // Name of prestige currency
-    baseResource: "", // Name of resource prestige is based on
+    baseResource: "beans", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
@@ -33,13 +33,13 @@ addLayer("B", {
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
-		points: new Decimal(0),
+		borii: new Decimal(0),
     }},
     color: "#32a148",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "big borii", // Name of prestige currency
     baseResource: "borii", // Name of resource prestige is based on
-    baseAmount() {return player.points}, // Get the current amount of baseResource
+    baseAmount() {return player.borii}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
